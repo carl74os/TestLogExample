ccbc96e6917b88178cc3d9c85397a975b39ab261
CarlOS <105883029+carl74os@users.noreply.github.com>
2023-03-21 13:57:05 +0100

    Logging functionality has been extended but it does not work correctly yet.

--- TestLog.py
+++ TestLog.py
@@ -1,19 +1,20 @@
 # coding: utf-8
 import shutil
 import time
+import signal
 
-from PyTools import PythonChecker, Logger
+from PyTools import PythonChecker, Logger, PyToolsBase
 
 MIN_PYTHON_VERSION = (3, 8)
-NAME = 'Unit Test Executor'
-DESCRIPTION = 'Executes unit tests and presents a summary at the end (Python >=3.8).'
-VERSION = '0.1.0.0 - 18.03.2023'
+NAME = 'Test Log Example'
+DESCRIPTION = 'This is an example which divides the terminal screen into a body and tail part and logs to these parts.'
+VERSION = '0.1.0.0 - 21.03.2023'
 NONE_TOKEN = 'N/A'
 
 LF = '\n'
 
 # Global logger variable
-logger_g = Logger()
+logger_g: Logger = Logger()
 
 
 # ######################################################################################################################
@@ -24,62 +25,110 @@ class Defines:
 
 
 # ######################################################################################################################
-def clearScr():
-    print('\033[2J')
+# Class for logging information, warnings and errors
+class Logger_(PyToolsBase):
+    # General information
+    _NAME = 'Logger'
+    _DESCRIPTION = 'The class provides method for logging information, warnings and errors to the console.'
+    _VERSION = '0.1.0.0 - 18.12.2020'
+
+    # ##################################################################################################################
+    def __init__(self, tail_size: int, border_char: str = '-'):
+        # Initialize the base class
+        PyToolsBase.__init__(self, self._NAME, self._VERSION, self._DESCRIPTION)
+
+        # Initialize the protected variables
+        self._tail_size: int = tail_size
+        self._border_char: str = border_char
+        self.__setUp__()
+
+        # Register the terminal resize signal
+        signal.signal(signal.SIGWINCH, self.__termResizedHdlr__)
+
+        # Clear the screen
+        self.clearScr()
+
+    # ######################################################################################################################
+    def clearScr(self):
+        self.__setUp__()
+        self.__scrollPageDown__(1)
+        self.__moveCur__(self._border_pos_y, 1)
+        logger_g.log('\033[2K' + self._border)
+        for idx in range(self._tail_size):
+            self.logTail(idx, '')
+
+    # ######################################################################################################################
+    def log(self, text: str):
+        # Check whether end of body window has been reached.
+        if len(self._body) >= self._body_size:
+            # Remove the first line of the body and scroll one line up
+            self._body.pop(0)
+            self.__moveCur__(self._scr_size_y, 1)
+
+            # Append the given text
+            self._body.append(text)
+
+            # Move the cursor to the top of the window and display the body, border and tailer
+            self.__moveCur__(self._body_pos_y, self._body_pos_x)
+            logger_g.log('\n'.join(['\033[2K' + line for line in self._body]))
+            logger_g.log('\033[2K' + self._border)
+            logger_g.log('\n'.join(['\033[2K' + line for line in self._tail]))
+        else:
+            # Append the given text
+            self._body.append(text)
+            
+            # Move the cursor to the top of the window and display the body
+            self.__moveCur__(self._body_pos_y, self._body_pos_x)
+            logger_g.log('\n'.join(['\033[2K' + line for line in self._body]))
+
+    # ######################################################################################################################
+    def logTail(self, idx: int, text):
+        # Check whether the tail index is in range. In this case log the tailer
+        if self._tail_size > idx:
+            self._tail[idx] = text
+            self.__moveCur__(self._tail_pos_y + idx, 1)
+            logger_g.log('\033[2K' + text)
+
+    # ##################################################################################################################
+    def progress(self, idx, count, total, suffix=''):
+        # Prepare the progress bar
+        bar_len = 60
+        filled_len = int(round(bar_len * count / float(total))) if total > 0 else 0
+
+        percents = round(100.0 * count / float(total), 1) if total > 0 else 0
+        bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+        # Log to tailer
+        self.logTail(idx, '[%s] %s%s  %s' % (bar, percents, '%', suffix))
+
+    # ######################################################################################################################
+    def __setUp__(self):
+        # Initialize the protected variables
+        (self._scr_size_x, self._scr_size_y) = shutil.get_terminal_size((80, 20))
+        self._body: list = list()
+        (self._body_pos_x, self._body_pos_y) = (1, 1)
+        self._body_size: int = self._scr_size_y - self._tail_size - 1
+        self._tail = [''] * self._tail_size
+        (self._tail_pos_x, self._tail_pos_y) = (1, (self._scr_size_y - self._tail_size - 1))
+        self._border: str = self._scr_size_x * self._border_char
+        self._border_pos_y = self._scr_size_y - self._tail_size - 2
+
+    # ######################################################################################################################
+    def __termResizedHdlr__(self, signum, frame):
+        # Reinitialize the logger
+        self.__setUp__()
+
+    # ######################################################################################################################
+    def __scrollPageDown__(self, nb_pages: int):
+        print('\033[%dT' % (self._scr_size_y * nb_pages))
+
+    # ######################################################################################################################
+    @staticmethod
+    def __moveCur__(row, column):
+        print("\033[%d;%dH" % (row, column))
 
 
-# ######################################################################################################################
-def scrollPageDown(nb_pages: int):
-    (size_x, size_y) = shutil.get_terminal_size((80, 20))
-    print('\033[%dT' % (size_y * nb_pages))
-
-
-# ######################################################################################################################
-def scrollLineDown(nb_lines: int):
-    print('\033[%dE' % nb_lines)
-
-
-# ######################################################################################################################
-def clearCurrLine():
-    print('\033[2K')
-
-
-# ######################################################################################################################
-def moveCur(row, column):
-    print("\033[%d;%dH" % (row, column))
-
-
-# ######################################################################################################################
-def updateLogWin(body: list, tail: list):
-    startTime: float = time.time()
-    end_offset: int = 3
-
-    # Calculate section positions
-    (size_x, size_y) = shutil.get_terminal_size((80, 20))
-    body_size: int = size_y - (1 + len(tail) + end_offset)
-    body_pos_x: int = 0
-    body_pos_y: int = 0
-    tail_pos_x: int = 0
-    tail_pos_y: int = body_size
-
-    # Prepare the section border string
-    sec_border: str = size_x * '-'
-
-    # Log body
-    body_idx: int = len(body) - body_size
-    if body_idx <= 0:
-        body_idx = 0
-    else:
-        moveCur(size_y, 0)
-    moveCur(body_pos_y, body_pos_x)
-    logger_g.log('\n'.join(['\033[2K' + line for line in body[body_idx:]]))
-
-    # Log tailer
-    moveCur(tail_pos_y, tail_pos_x)
-    logger_g.log('\033[2K' + sec_border)
-    logger_g.log('\n'.join(['\033[2K' + line for line in tail]))
-    logger_g.log('Duration: {:6f}'.format(time.time() - startTime))
-
+logger1_g: Logger_ = Logger_(3)
 
 # ######################################################################################################################
 
@@ -88,15 +137,9 @@ if __name__ == "__main__":
     # Check interpreter
     PythonChecker().check(MIN_PYTHON_VERSION)
 
-    # Scroll one page down to clear the screen
-    scrollPageDown(1)
-
-    body: list = list()
-    tail: list = ['', '', '']
-    for idx in range(100):
-        updateLogWin(body, tail)
-        body.append('{0} BODY'.format(idx))
-        tail.pop(0)
-        tail.append('{0} TAIL'.format(idx))
+    for idx in range(1000):
+        logger1_g.log('{0} BODY'.format(idx))
+        logger1_g.progress(0, idx, 1000, 'Performing...')
+        logger1_g.logTail(1, 'Index: {0}'.format(idx))
+        logger1_g.logTail(2, 'Index * 3: {0}'.format(3 * idx))
         time.sleep(0.05)
-
