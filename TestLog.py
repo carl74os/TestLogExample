b3a26dbd463c26bde0e58a096a109560a3794bf6
CarlOS <105883029+carl74os@users.noreply.github.com>
2023-03-21 22:08:51 +0100

    Update TestLog.py
    Printing the body and the tailer has been optimized.

--- TestLog.py
+++ TestLog.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import shutil
+import sys
 import time
 import signal
 
@@ -32,6 +33,9 @@ class Logger_(PyToolsBase):
     _DESCRIPTION = 'The class provides method for logging information, warnings and errors to the console.'
     _VERSION = '0.1.0.0 - 18.12.2020'
 
+    # Constants
+    TERM_OFFSET: int = 0
+
     # ##################################################################################################################
     def __init__(self, tail_size: int, border_char: str = '-'):
         # Initialize the base class
@@ -40,6 +44,8 @@ class Logger_(PyToolsBase):
         # Initialize the protected variables
         self._tail_size: int = tail_size
         self._border_char: str = border_char
+        self._body: list = list()
+        self._tail = [''] * self._tail_size
         self.__setUp__()
 
         # Register the terminal resize signal
@@ -50,44 +56,45 @@ class Logger_(PyToolsBase):
 
     # ######################################################################################################################
     def clearScr(self):
+        self._body: list = list()
+        self._tail = [''] * self._tail_size
         self.__setUp__()
         self.__scrollPageDown__(1)
-        self.__moveCur__(self._border_pos_y, 1)
-        logger_g.log('\033[2K' + self._border)
-        for idx in range(self._tail_size):
-            self.logTail(idx, '')
+        self.__moveCur__(self._border_pos_y, self.TERM_OFFSET)
+        log_str: str = self._border + '\n'
+        log_str += '\n'.join(['\033[2K' + line for line in self._tail])
+        self.__log__(log_str)
 
     # ######################################################################################################################
     def log(self, text: str):
+        # Append the given text
+        self._body.append(text)
+
         # Check whether end of body window has been reached.
         if len(self._body) >= self._body_size:
             # Remove the first line of the body and scroll one line up
             self._body.pop(0)
-            self.__moveCur__(self._scr_size_y, 1)
-
-            # Append the given text
-            self._body.append(text)
+            self.__moveCur__(self._scr_size_y, self.TERM_OFFSET)
 
             # Move the cursor to the top of the window and display the body, border and tailer
-            self.__moveCur__(self._body_pos_y, self._body_pos_x)
-            logger_g.log('\n'.join(['\033[2K' + line for line in self._body]))
-            logger_g.log('\033[2K' + self._border)
-            logger_g.log('\n'.join(['\033[2K' + line for line in self._tail]))
+            log_str: str = '\n'.join(['\033[2K' + line for line in self._body])
+            log_str += '\n' + self._border + '\n'
+            log_str += '\n'.join(['\033[2K' + line for line in self._tail])
         else:
-            # Append the given text
-            self._body.append(text)
-            
-            # Move the cursor to the top of the window and display the body
-            self.__moveCur__(self._body_pos_y, self._body_pos_x)
-            logger_g.log('\n'.join(['\033[2K' + line for line in self._body]))
+            # Prepare the log string
+            log_str: str = '\n'.join(['\033[2K' + line for line in self._body])
+
+        # Move the cursor to the top of the window and display the body
+        self.__moveCur__(self._body_pos_y, self.TERM_OFFSET)
+        self.__log__(log_str)
 
     # ######################################################################################################################
     def logTail(self, idx: int, text):
         # Check whether the tail index is in range. In this case log the tailer
         if self._tail_size > idx:
             self._tail[idx] = text
-            self.__moveCur__(self._tail_pos_y + idx, 1)
-            logger_g.log('\033[2K' + text)
+            self.__moveCur__(self._tail_pos_y + idx, self.TERM_OFFSET)
+            self.__log__('\033[2K' + text)
 
     # ##################################################################################################################
     def progress(self, idx, count, total, suffix=''):
@@ -105,13 +112,11 @@ class Logger_(PyToolsBase):
     def __setUp__(self):
         # Initialize the protected variables
         (self._scr_size_x, self._scr_size_y) = shutil.get_terminal_size((80, 20))
-        self._body: list = list()
-        (self._body_pos_x, self._body_pos_y) = (1, 1)
+        self._body_pos_y: int = self.TERM_OFFSET
         self._body_size: int = self._scr_size_y - self._tail_size - 1
-        self._tail = [''] * self._tail_size
-        (self._tail_pos_x, self._tail_pos_y) = (1, (self._scr_size_y - self._tail_size - 1))
+        self._tail_pos_y: int = self._body_size + 1
         self._border: str = self._scr_size_x * self._border_char
-        self._border_pos_y = self._scr_size_y - self._tail_size - 2
+        self._border_pos_y = self._body_size
 
     # ######################################################################################################################
     def __termResizedHdlr__(self, signum, frame):
@@ -125,7 +130,17 @@ class Logger_(PyToolsBase):
     # ######################################################################################################################
     @staticmethod
     def __moveCur__(row, column):
-        print("\033[%d;%dH" % (row, column))
+        print('\033[%d;%dH' % (row, column))
+
+    # ##################################################################################################################
+    @staticmethod
+    def __log__(text):
+        """
+        :param text:    Text to be logged.
+        :return:        None
+        """
+        sys.stdout.write(text)
+        sys.stdout.flush()
 
 
 logger1_g: Logger_ = Logger_(3)
@@ -137,9 +152,12 @@ if __name__ == "__main__":
     # Check interpreter
     PythonChecker().check(MIN_PYTHON_VERSION)
 
+    body_idx: int = 0
     for idx in range(1000):
-        logger1_g.log('{0} BODY'.format(idx))
-        logger1_g.progress(0, idx, 1000, 'Performing...')
-        logger1_g.logTail(1, 'Index: {0}'.format(idx))
+        if (idx % 1) == 0:
+            logger1_g.log('{0} BODY'.format(body_idx))
+            body_idx += 1
+        logger1_g.progress(1, idx, 1000, 'Performing...')
+        logger1_g.logTail(0, 'Index: {0}'.format(idx))
         logger1_g.logTail(2, 'Index * 3: {0}'.format(3 * idx))
         time.sleep(0.05)
